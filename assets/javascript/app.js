7c5b64e7be741235cdb547742ee36c017c0091c8
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-08 11:47:08 -0600

    began populating divs with elements, thinking through click and timer logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,106 @@
+$(document).ready(function () {
+
+    var counter = 15;
+    var isRunning = false;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+    var triviaGame = {
+
+        question1: {
+            question: "What injury did Margaret Hamilton endure while filming the classic 1939 movie?",
+            correctAnswer: "Bad burns",
+            otherAnswers: ["Broken leg", "Puncture wound from a piece of the flying monkey's costume", "Bitten by a dog"],
+            answerInfo: "Bad Burns - Margaret Hamilton suffered severe burns while filming her “puff of smoke” scene and the cameras kept on filming. As Hamilton went below the stage, she was trapped in the elevator with the fire. Dorthy and Glinda kept acting above her none the wiser about what was going on below. ",
+            // picture: <iframe src="https://giphy.com/embed/ihBXdfRh5uJOM" width="480" height="356" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ihBXdfRh5uJOM">via GIPHY</a></p>,
+        },
+
+        question2: {
+            question: "What did the real life “Dorothy” say about Frank L. Baum’s book?",
+            correctAnswer: "Nothing at all",
+            otherAnswers: ['It was the “most flattering” thing someone could have done.', 'She was outraged and demanded he change the name of the character.', 'It was the “most enduring” thing someone could have done.'],
+            answerInfo: "Nothing at all - Dorthy was named after Baum’s niece who passed away at just five months old."
+            // picture: <iframe src="https://giphy.com/embed/AldqHdVi1m7Be" width="480" height="448" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-wizard-of-oz-woz-AldqHdVi1m7Be">via GIPHY</a></p>,
+
+        },
+
+        question3: {
+            question: "What color was the original Emerald City?",
+            correctAnswer: "White",
+            otherAnswers: ["pink", "green", "yellow"],
+            answerInfo: "White - In Baum’s book, the city is white. The inhabitants of the emerald city only see it as green because of special green spectacles the Wizard forces them to wear. ",
+            // picture: <iframe src="https://giphy.com/embed/dsSndFyw5bjIk" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/wizard-of-oz-emerald-city-dsSndFyw5bjIk">via GIPHY</a></p>,
+        }
+
+    }
+
+
+$("#start-button").on("click", function ()  {
+
+        $("#buttons").empty();
+
+        $("#location-status").text("Here we go ...");
+        
+        $("#question-area").append(triviaGame.question1.question);
+      
+        $("#a").text(triviaGame.question1.otherAnswers[0]);
+     
+        $("#b").text(triviaGame.question1.otherAnswers[1]);
+     
+        $("#c").text(triviaGame.question1.correctAnswer);
+
+        $("#c").addClass("correct");
+     
+        $("#d").text(triviaGame.question1.otherAnswers[2]);
+
+        var timer = setInterval(countDown, 1000);
+
+        var countDown = function () {
+            counter--;
+            $("#timer").html("00:" + counter);
+    
+        }
+
+       var stopCountDown = function () {
+
+            clearInterval(timer, 3000);
+        }
+      
+   
+$("button").on("click", function() {
+
+   clearInterval(timer, 3000);
+
+    if ($(this).hasClass("correct")) {
+    
+
+}
+   
+})
+
+})
+
+})
+
+
+// GIF 1
+//     < iframe src = "https://giphy.com/embed/ihBXdfRh5uJOM" width = "480" height = "356" frameBorder = "0" class="giphy-embed" allowFullScreen ></iframe > <p><a href="https://giphy.com/gifs/ihBXdfRh5uJOM">via GIPHY</a></p>
+
+// GIF 2
+//     < iframe src = "https://giphy.com/embed/AldqHdVi1m7Be" width = "480" height = "448" frameBorder = "0" class="giphy-embed" allowFullScreen ></iframe > <p><a href="https://giphy.com/gifs/film-wizard-of-oz-woz-AldqHdVi1m7Be">via GIPHY</a></p>
+
+// GIF 3
+//     < iframe src = "https://giphy.com/embed/dsSndFyw5bjIk" width = "480" height = "255" frameBorder = "0" class="giphy-embed" allowFullScreen ></iframe > <p><a href="https://giphy.com/gifs/wizard-of-oz-emerald-city-dsSndFyw5bjIk">via GIPHY</a></p>
+
+// GIF 4
+//     < iframe src = "https://giphy.com/embed/1y0tWGimBNYHu" width = "480" height = "480" frameBorder = "0" class="giphy-embed" allowFullScreen ></iframe > <p><a href="https://giphy.com/gifs/wizard-of-oz-judy-garland-dorothy-gale-1y0tWGimBNYHu">via GIPHY</a></p>
+
+// GIF 5
+//     < iframe src = "https://giphy.com/embed/YHPoWDuPAOREI" width = "480" height = "480" frameBorder = "0" class="giphy-embed" allowFullScreen ></iframe > <p><a href="https://giphy.com/gifs/wizard-of-oz-judy-garland-the-YHPoWDuPAOREI">via GIPHY</a></p>
+
+
+
 // once user presses the start button, 
 
 // timer should start 
@@ -17,55 +120,3 @@
 // else user does not choose an answer - 
 // should show losing message and picture with correct answer after timer runs to zero. 
 // add one to unanswered count
-
-
-$(document).ready(function() {
-
-    var counter = 30;
-    var isRunning = false;
-    var correct = 0;
-    var incorrect = 0;
-    var unanswered = 0;
-
-
-
-function countDown () {
-
-    counter--;
-    $("#timer").html("00:" + counter);
-
-}
-
-
-
-
-    $("#start-button").on("click", function() {
-        $("#location-status").text("Here we go ...")
-        $("#buttons").empty();
-        var counterId = setInterval(countDown, 1000);
-        questions()
-    })
-
-
-    function questions() {
-
-        
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
-
-
-
