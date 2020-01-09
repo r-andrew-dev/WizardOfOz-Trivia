7acab4f09d02b5834169e7b1b77e04782620c667
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-09 14:20:12 -0600

    fixedquestion function values to be dependent on N, should allow subsequent question info to populate without having to re-type.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,30 +9,32 @@ $(document).ready(function () {
 
     var triviaGame = {
 
-        question1: {
+        1: {
             question: "What injury did Margaret Hamilton endure while filming the classic 1939 movie?",
             otherAnswers: ["Broken leg", "Puncture wound from a piece of the flying monkey's costume", "Bad Burns", "Bitten by a dog"],
-            // correctAnswer: triviaGame.this.otherAnswers[2],
+            correctAnswer: 2,
             answerInfo: "Bad Burns - Margaret Hamilton suffered severe burns while filming her “puff of smoke” scene and the cameras kept on filming. As Hamilton went below the stage, she was trapped in the elevator with the fire. Dorthy and Glinda kept acting above her none the wiser about what was going on below. ",
             // picture: <iframe src="https://giphy.com/embed/ihBXdfRh5uJOM" width="480" height="356" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ihBXdfRh5uJOM">via GIPHY</a></p>,
         },
 
-        question2: {
+        2: {
             question: "What did the real life “Dorothy” say about Frank L. Baum’s book?",
-            // correctAnswer: triviaGame.question2.otherAnswers[1],
+            correctAnswer: 1,
             otherAnswers: ['It was the “most flattering” thing someone could have done.', "Nothing at all", 'She was outraged and demanded he change the name of the character.', 'It was the “most enduring” thing someone could have done.'],
             answerInfo: "Nothing at all - Dorthy was named after Baum’s niece who passed away at just five months old."
             // picture: <iframe src="https://giphy.com/embed/AldqHdVi1m7Be" width="480" height="448" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-wizard-of-oz-woz-AldqHdVi1m7Be">via GIPHY</a></p>,
 
         },
 
-        question3: {
+        3: {
             question: "What color was the original Emerald City?",
-            // correctAnswer: triviaGame.question3.otherAnswers[1],
+            correctAnswer: 1,
             otherAnswers: ["pink", "white", "green", "yellow"],
             answerInfo: "In Baum’s book, the city is white. The inhabitants of the emerald city only see it as green because of special green spectacles the Wizard forces them to wear. ",
             // picture: <iframe src="https://giphy.com/embed/dsSndFyw5bjIk" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/wizard-of-oz-emerald-city-dsSndFyw5bjIk">via GIPHY</a></p>,
-        }
+        },
+
+
 
     }
 
@@ -44,21 +46,21 @@ $(document).ready(function () {
 
     var timer = function() {setInterval(countDown, 1000)};
 
-    var Questions = function (N) {
+    var Questions = function () {
 
         $("#buttons").empty();
 
         $("#location-status").text("Here we go ...");
         
-        $("#question-area").append(triviaGame.question(N).question);
+        $("#question-area").append(triviaGame[N].question);
       
-        $("#a").text(triviaGame.question(N).otherAnswers[0]);
+        $("#a").text(triviaGame[N].otherAnswers[0]);
      
-        $("#b").text(triviaGame.questionN.otherAnswers[1]);
+        $("#b").text(triviaGame[N].otherAnswers[1]);
      
-        $("#c").text(triviaGame.questionN.otherAnswers[2]);
+        $("#c").text(triviaGame[N].otherAnswers[2]);
      
-        $("#d").text(triviaGame.questionN.otherAnswers[3]);
+        $("#d").text(triviaGame[N].otherAnswers[3]);
 
         i++;
 
