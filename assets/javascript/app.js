47a93e8a37b9f424d559cab4d14b338478510f72
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-09 01:18:22 -0600

    styled content, issue with accessing content to resolve, end of working session

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,21 +5,22 @@ $(document).ready(function () {
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
+    var N = 1
 
     var triviaGame = {
 
         question1: {
             question: "What injury did Margaret Hamilton endure while filming the classic 1939 movie?",
-            correctAnswer: "Bad burns",
-            otherAnswers: ["Broken leg", "Puncture wound from a piece of the flying monkey's costume", "Bitten by a dog"],
+            otherAnswers: ["Broken leg", "Puncture wound from a piece of the flying monkey's costume", "Bad Burns", "Bitten by a dog"],
+            // correctAnswer: triviaGame.this.otherAnswers[2],
             answerInfo: "Bad Burns - Margaret Hamilton suffered severe burns while filming her “puff of smoke” scene and the cameras kept on filming. As Hamilton went below the stage, she was trapped in the elevator with the fire. Dorthy and Glinda kept acting above her none the wiser about what was going on below. ",
             // picture: <iframe src="https://giphy.com/embed/ihBXdfRh5uJOM" width="480" height="356" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ihBXdfRh5uJOM">via GIPHY</a></p>,
         },
 
         question2: {
             question: "What did the real life “Dorothy” say about Frank L. Baum’s book?",
-            correctAnswer: "Nothing at all",
-            otherAnswers: ['It was the “most flattering” thing someone could have done.', 'She was outraged and demanded he change the name of the character.', 'It was the “most enduring” thing someone could have done.'],
+            // correctAnswer: triviaGame.question2.otherAnswers[1],
+            otherAnswers: ['It was the “most flattering” thing someone could have done.', "Nothing at all", 'She was outraged and demanded he change the name of the character.', 'It was the “most enduring” thing someone could have done.'],
             answerInfo: "Nothing at all - Dorthy was named after Baum’s niece who passed away at just five months old."
             // picture: <iframe src="https://giphy.com/embed/AldqHdVi1m7Be" width="480" height="448" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-wizard-of-oz-woz-AldqHdVi1m7Be">via GIPHY</a></p>,
 
@@ -27,58 +28,58 @@ $(document).ready(function () {
 
         question3: {
             question: "What color was the original Emerald City?",
-            correctAnswer: "White",
-            otherAnswers: ["pink", "green", "yellow"],
-            answerInfo: "White - In Baum’s book, the city is white. The inhabitants of the emerald city only see it as green because of special green spectacles the Wizard forces them to wear. ",
+            // correctAnswer: triviaGame.question3.otherAnswers[1],
+            otherAnswers: ["pink", "white", "green", "yellow"],
+            answerInfo: "In Baum’s book, the city is white. The inhabitants of the emerald city only see it as green because of special green spectacles the Wizard forces them to wear. ",
             // picture: <iframe src="https://giphy.com/embed/dsSndFyw5bjIk" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/wizard-of-oz-emerald-city-dsSndFyw5bjIk">via GIPHY</a></p>,
         }
 
     }
 
+    var countDown = function () {
+        counter--;
+        $("#timer").html("00:" + counter);
 
-$("#start-button").on("click", function ()  {
+    }
+
+    var timer = function() {setInterval(countDown, 1000)};
+
+    var Questions = function (N) {
 
         $("#buttons").empty();
 
         $("#location-status").text("Here we go ...");
         
-        $("#question-area").append(triviaGame.question1.question);
+        $("#question-area").append(triviaGame.question(N).question);
       
-        $("#a").text(triviaGame.question1.otherAnswers[0]);
+        $("#a").text(triviaGame.question(N).otherAnswers[0]);
      
-        $("#b").text(triviaGame.question1.otherAnswers[1]);
+        $("#b").text(triviaGame.questionN.otherAnswers[1]);
      
-        $("#c").text(triviaGame.question1.correctAnswer);
-
-        $("#c").addClass("correct");
+        $("#c").text(triviaGame.questionN.otherAnswers[2]);
      
-        $("#d").text(triviaGame.question1.otherAnswers[2]);
+        $("#d").text(triviaGame.questionN.otherAnswers[3]);
 
-        var timer = setInterval(countDown, 1000);
+        i++;
 
-        var countDown = function () {
-            counter--;
-            $("#timer").html("00:" + counter);
-    
-        }
+        timer();
 
-       var stopCountDown = function () {
+    }
 
-            clearInterval(timer, 3000);
-        }
-      
+
+
+
+$("#start-button").on("click", function ()  {
+
+     Questions();
+
    
-$("button").on("click", function() {
+// $(".answer").on("click", function() {
 
-   clearInterval(timer, 3000);
+// //    clearInterval(timer, 3000);
 
-    if ($(this).hasClass("correct")) {
-    
-
-}
+//     if ($(this).hasClass("correct")) {
    
-})
-
 })
 
 })
